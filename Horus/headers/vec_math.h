28c3973c196cc422530871d36b2d057ad8ddcacc
Your Name <mauriziodiv@hotmail.it>
2025-12-26 16:30:03 +0000

    improvement of math operations

--- Horus/headers/vec_math.h
+++ Horus/headers/vec_math.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <iostream>
+#include <cmath>
 
 template <typename T> class Point
 {
@@ -56,6 +56,20 @@ public:
 		return std::sqrt((x * x) + (y * y) + (z * z));
 	}
 
+	void normalize()
+	{
+		T length = getLength();
+
+		if (length > 0.0001)
+		{
+			T c = 1.0f / length;
+
+			x = x * c;
+			y = y * c;
+			z = z * c;
+		}
+	}
+
 	Vector3D operator+(const Vector3D& v) const
 	{
 		return Vector3D(x + v.getX(), y + v.getY(), z + v.getZ());
@@ -71,6 +85,22 @@ public:
 		return Vector3D(x * v, y * v, z  * v);
 	}
 
+	// Dot product
+	T operator* (const Vector3D<T>& v) const
+	{
+		return (x * v.getX()) + (y * v.getY()) + (z * v.getZ());
+	}
+
+	// Cross product
+	Vector3D operator|(const Vector3D<T>& v) const
+	{
+		return Vector3D(
+			(y * v.getZ()) - (z * v.getY()),
+			(z * v.getX()) - (x * v.getZ()),
+			(x * v.getY()) - (y * v.getX())
+		);
+	}
+
 	Vector3D operator/(T v) const
 	{
 		return Vector3D(x / v, y / v, z / v);
@@ -81,4 +111,106 @@ private:
 	T y;
 	T z;
 
+};
+
+template <typename T> class Matrix4X4
+{
+	public:
+		Matrix4X4() {
+		
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					i == j ? m4x4[i][j] = T(1) : m4x4[i][j] = T(0);
+				}
+			}
+		}
+
+		void setValue(int x, int y, T value)
+		{
+			m4x4[x][y] = value;
+		}
+
+		T getValue(int x, int y)
+		{
+			return m4x4[x][y];
+		}
+
+		Matrix4X4 operator* (const Matrix4X4& m) const
+		{
+			Matrix4X4 result;
+
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					T sum = T(0);
+
+					for (int n = 0; n < 4; n++)
+					{
+						sum += m4x4[i][n] * m.getValue(n, j);
+					}
+					result.setValue(i, j, sum);
+				}
+			}
+			return result;
+		}
+
+		Vector3D<T> operator* (const Vector3D<T>& v) const
+		{
+			T x = (m4x4[0][0] * v.getX()) + (m4x4[0][1] * v.getY()) + (m4x4[0][2] * v.getZ()) + m4x4[0][3];
+			T y = (m4x4[1][0] * v.getX()) + (m4x4[1][1] * v.getY()) + (m4x4[1][2] * v.getZ()) + m4x4[1][3];
+			T z = (m4x4[2][0] * v.getX()) + (m4x4[2][1] * v.getY()) + (m4x4[2][2] * v.getZ()) + m4x4[2][3];
+
+			return Vector3D<T>(x, y, z);
+		}
+
+		static Matrix4X4 rotationX(T angle)
+		{
+			Matrix4X4 result;
+
+			T c = std::cos(angle);
+			T s = std::sin(angle);
+
+			result.setValue(1, 1, c);
+			result.setValue(1, 2, -s);
+			result.setValue(2, 1, s);
+			result.setValue(2, 2, c);
+
+			return result;
+		}
+
+		static Matrix4X4 rotationY(T angle)
+		{
+			Matrix4X4 result;
+
+			T c = std::cos(angle);
+			T s = std::sin(angle);
+
+			result.setValue(0, 0, c);
+			result.setValue(0, 2, s);
+			result.setValue(2, 0, -s);
+			result.setValue(2, 2, c);
+
+			return result;
+		}
+
+		static Matrix4X4 rotationZ(T angle)
+		{
+			Matrix4X4 result;
+
+			T c = std::cos(angle);
+			T s = std::sin(angle);
+
+			result.setValue(0, 0, c);
+			result.setValue(0, 1, -s);
+			result.setValue(1, 0, s);
+			result.setValue(1, 1, c);
+
+			return result;
+		}
+
+	private:
+		T m4x4[4][4];
 };
